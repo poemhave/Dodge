b5032fbd2941918e3b19b56a3b4571f563cb968d
poemhave <1012m@naver.com>
2022-01-21 12:14:14 +0900

    220121

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody; //이동에 사용할 리지드바디 컴포넌트
+    public float speed = 8f; //이동속력
+
+  void Start() //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+            //위쪽 방향키 입력이 감지된 경우 z 방향 힘주기
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        //아래쪽 방향키 입력이 감지된 경우 -z 방향 힘주기
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        //오른쪽 방향키 입력이 감지된 경우 x 방향 힘주기
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        //왼쪽 방향키 입력이 감지된 경우 -x 방향 힘주기
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+    }
+
+    public void Die()
+    //자신의 게임 오브젝트를 비활성화
+    {
+        gameObject.SetActive(false);
+    }
+}
